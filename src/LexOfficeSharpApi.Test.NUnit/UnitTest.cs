92ac32194ed5dbda595ecbe698cd9bbda4aaf3b9
Andreas <andy.reitberger@gmail.com>
2024-04-16 06:39:37 +0200

    Updated `Test` project
    Fixed #10

--- src/LexOfficeSharpApi.Test.NUnit/UnitTest.cs
+++ src/LexOfficeSharpApi.Test.NUnit/UnitTest.cs
@@ -1,32 +1,40 @@
 using AndreasReitberger.API.LexOffice;
 using AndreasReitberger.Core.Utilities;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.ObjectModel;
 using System.Security;
-using System.Threading.Tasks;
 
-namespace LexOfficeSharpApiTest
+namespace LexOfficeSharpApi.Test.NUnit
 {
-    [TestClass]
-    public class LexOfficeSharpApiTest
+    public class Tests
     {
-        // https://docs.microsoft.com/en-us/dotnet/core/tutorials/testing-library-with-visual-studio
-
         private const string tokenString = "YOUR_TOKEN";
-        [TestMethod]
-        public async Task TestGetInvoices()
+
+        [SetUp]
+        public void Setup()
         {
-            SecureString token = SecureStringHelper.ConvertToSecureString(tokenString);
-            LexOfficeClient handler = new LexOfficeClient(token);
-
-            var invoicesList = await handler.GetInvoiceListAsync(LexVoucherStatus.open);
-            var invoices = await handler.GetInvoicesAsync(invoicesList);
-
-            Assert.IsTrue(invoices != null && invoices.Count > 0);
         }
 
+        [Test]
+        public async Task TestGetInvoices()
+        {
+            try
+            {
+                SecureString token = SecureStringHelper.ConvertToSecureString(tokenString);
+                LexOfficeClient handler = new(token);
+
+                ObservableCollection<VoucherListContent> invoicesList = await handler.GetInvoiceListAsync(LexVoucherStatus.open);
+                ObservableCollection<LexQuotation> invoices = await handler.GetInvoicesAsync(invoicesList);
+
+                Assert.IsTrue(invoices != null && invoices.Count > 0);
+            }
+            catch (Exception ex) 
+            {           
+                Assert.Fail(ex.Message);
+            }
+        }
 
         /*
-        [TestMethod]
+        [Test]
         public async Task TestGetQuotations()
         {
             SecureString token = SecureStringHelper.ConvertToSecureString(tokenString);
@@ -38,7 +46,7 @@ namespace LexOfficeSharpApiTest
         }
         
          
-        [TestMethod]
+        [Test]
         public async Task TestGetContacts()
         {
             SecureString token = SecureStringHelper.ConvertToSecureString(tokenString);
@@ -48,7 +56,7 @@ namespace LexOfficeSharpApiTest
             Assert.IsTrue(contacts != null && contacts.Count > 0);
         }
 
-        [TestMethod]
+        [Test]
         public async Task TestGetContact()
         {
             SecureString token = SecureStringHelper.ConvertToSecureString(tokenString);
